2b3581434c4938235b895323c17cf3d2f3147086
joaomtripoli <joaomtripoli@gmail.com>
2021-08-25 18:24:50 -0300

    cleaning the home page

--- src/views/HomePage/HomePage.tsx
+++ src/views/HomePage/HomePage.tsx
@@ -1,21 +1,7 @@
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router';
-
-import { Button } from 'shared/components';
 import Wrapper from 'shared/components/Layouts/Wrapper';
-import { authActions } from 'shared/store';
 import { Hero } from './styled';
 
 export const HomePage = () => {
-  const history = useHistory();
-
-  const redirect = () => history.push('/login');
-  const dispatch = useDispatch();
-
-  const handleLogout = () => {
-    dispatch(authActions.signOut());
-  };
-
   return (
     <Wrapper>
       <Hero></Hero>
