4405100f67ffb501116b08a09e2ae43f9315268e
João Matheus Tripoli <43175872+joaomtripoli@users.noreply.github.com>
2022-08-07 20:27:47 -0300

    fix upgrade to react v18 problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './views/App';
 import { store } from './shared/store';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container as Element);
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
